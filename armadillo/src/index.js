1ad8cd3a54659b3325ed7dba2d343e1282623a5c
Philipp Otto <philipp.otto2@student.hpi.de>
2018-05-07 13:22:25 +0200

    Work on armadillo setup

--- armadillo/src/index.js
+++ armadillo/src/index.js
@@ -1,13 +1,35 @@
-import Modeler from 'bpmn-js/lib/Modeler'
-import diagramXML from '../resources/newDiagram.bpmn';
+import Viewer from 'bpmn-js/lib/Viewer'
+import diagramXML from '../resources/two_lanes.bpmn';
 
-const modeler = new Modeler({ container: '#canvas' })
+const viewer = new Viewer({
+    container: '#canvas',
+    height: 400
+ })
 
-modeler.importXML(diagramXML, err => {
+function handleTaskClick(id) {
+    console.log('click on ' + id);
+}
+
+viewer.importXML(diagramXML, err => {
     if (err) {
         // :(
     } else {
-        const canvas = modeler.get('canvas')
-        canvas.zoom('fit-viewport')
+        const canvas = viewer.get('canvas')
+        //canvas.zoom('fit-viewport')
+
+        var eventBus = viewer.get('eventBus');
+        var events = [
+            'element.click',
+          ];
+
+        events.forEach(function(event) {
+            eventBus.on(event, function(e) {
+              // e.element = the model element
+              // e.gfx = the graphical element
+              if (e.element.type == 'bpmn:Task') {
+                handleTaskClick(e.element.id)
+              }
+            });
+          });
     }
 })
