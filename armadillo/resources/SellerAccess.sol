6ea8ddfcf80cb826b0350914d8f1e81cdff009ef
Philipp Otto <philipp.otto2@student.hpi.de>
2018-06-13 16:39:19 +0200

    Implement access contract and add cmd tool to change access rights

--- armadillo/resources/SellerAccess.sol
+++ armadillo/resources/SellerAccess.sol
@@ -1,7 +1,45 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.23;
 
 contract SellerAccess {
-    function isAuthorized(address buyerAddress) public returns(bool) {
-        return buyerAddress == 0xc5fdf4076b8f3a5357c5e395ab970b5b54098fef;
+    address owner;
+    mapping(string => uint) roleToIndex;
+    mapping(address => uint) addressToAccessBitmask;
+
+    uint currentIndex = 1;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier only_owner() {
+        require(owner == msg.sender);
+        _;
+    }
+
+    function isAuthorized(address buyerAddress, string _role) public view returns(bool) {
+        if (roleToIndex[_role] == 0) {
+            return false;
+        }
+
+        uint roleIndex = roleToIndex[_role] - 1;
+
+        return (addressToAccessBitmask[buyerAddress] & (1 << roleIndex)) != 0;
+    }
+
+    function giveRights(address _receiver, string _role) public only_owner {
+        if (roleToIndex[_role] == 0) {
+            roleToIndex[_role] = currentIndex;
+            currentIndex += 1;
+        }
+
+        uint roleIndex = roleToIndex[_role] - 1;
+
+        addressToAccessBitmask[_receiver] = addressToAccessBitmask[_receiver] | (1 << roleIndex);
+    }
+
+    function removeRights(address _receiver, string _role) public only_owner {
+        uint roleIndex = roleToIndex[_role] - 1;
+
+        addressToAccessBitmask[_receiver] = addressToAccessBitmask[_receiver] & ~(1 << roleIndex);
     }
 }
